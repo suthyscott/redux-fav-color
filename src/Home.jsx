b16d95fbb4fb3f5f4bc265f7154674252a9e6bfc
suthyscott <sutherlandscott21@gmail.com>
2023-05-22 15:20:58 -0600

    added more setup in Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
+  const handleChangeColor = () => {
+    navigate('/')
+  }
   return (
     <main>
       <h2>Your favorite color is below:</h2>
-      <p>[Insert favorite color here]</p>
+      <p>[insert favorite color here]</p>
+      <button onClick={handleChangeColor}>Change Color</button>
     </main>
   )
 }
