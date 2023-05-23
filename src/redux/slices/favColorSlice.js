d858fd43ce911a5a5f4725463787345d732cb951
suthyscott <sutherlandscott21@gmail.com>
2023-05-23 16:00:44 -0600

    started on slices

---
+++ src/redux/slices/favColorSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const favColorSlice = createSlice({
+    name: 'favColor',
+    initialState: {
+        value: ''
+    },
+    reducers: {
+        setColor: (state, action) => {
+            state.value = action.payload
+        },
+        clearColor: (state, action) => {
+            state.value = ''
+        }
+    }
+})
+
+console.log(favColorSlice)
+// export const setColor  = favColorSlice.actions.setColor
+// export const clearColor  = favColorSlice.actions.clearColor
+
+export const {setColor, clearColor} = favColorSlice.actions
+
+export default favColorSlice.reducer
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from '@reduxjs/toolkit'
+import favColorSliceReducer from './slices/favColorSlice'
+
+const store = configureStore({
+    reducer: {favColor: favColorSliceReducer}
+})
+
+export default store
